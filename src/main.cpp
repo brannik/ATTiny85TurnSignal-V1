06914743d8fe28efa83891939c67cfd4e553c157
Georgi Golemshinski <georgi.vtu.mi@gmail.com>
2023-09-04 01:33:28 -0700

    cosmetic updates

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "config.h"
 #include "timeObj.h"
 
+
 int TurnSignalState = LOW;
 int StopSignalState = LOW;
 int RunSignalState = LOW;
@@ -26,7 +27,7 @@ bool triggerOnce = true;
 void timeLoop (unsigned long int startMillis, unsigned long int interval) { // delay substitute function
   while (millis() - startMillis < interval) {}
 }
-
+// FUNCTIONS
 void StartUpAnimation(){
   for(int i=0;i<=SS_LED_NUMBER;i++){
     for(int y=0;y<SS_LED_NUMBER-i;y++){
@@ -53,34 +54,9 @@ void StartUpAnimation(){
   FastLED[RUN_INDEX].showColor(CRGB::Black);
   
 }
-
-void setup() {
-  pinMode(TURNS_INPUT_PIN,INPUT);
-  pinMode(STOPS_INPUT_PIN,INPUT);
-  pinMode(RUNS_INPUT_PIN,INPUT);
-
-  pinMode(TURNS_OUTPUT_PIN,OUTPUT);
-  pinMode(STOPS_OUTPUT_PIN,OUTPUT);
-  pinMode(RUNS_OUTPUT_PIN,OUTPUT);
-
-  FastLED.addLeds<LED_TYPE, TURNS_OUTPUT_PIN, COLOR_ORDER>(TurnSignalStrip, TS_LED_NUMBER).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE, STOPS_OUTPUT_PIN, COLOR_ORDER>(StopSignalStrip, SS_LED_NUMBER).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<LED_TYPE, RUNS_OUTPUT_PIN, COLOR_ORDER>(RunningStrip, RS_LED_NUMBER).setCorrection(TypicalLEDStrip);
-
-  fill_solid(TurnSignalStrip,TS_LED_NUMBER,CRGB::Black);
-  fill_solid(StopSignalStrip,SS_LED_NUMBER,CRGB::Black);
-  fill_solid(RunningStrip,RS_LED_NUMBER,CRGB::Black);
-
-  StartUpAnimation();
-}
-
-void loop() {
-  TurnSignalState = digitalRead(TURNS_INPUT_PIN);
-  StopSignalState = digitalRead(STOPS_INPUT_PIN);
-  RunSignalState = digitalRead(RUNS_INPUT_PIN);
-
-  if(TurnSignalState == HIGH){
-    for(int dot=0;dot<TS_LED_NUMBER;dot++){
+void TurnSignalAnimationON(){
+  // follow animation
+  for(int dot=0;dot<TS_LED_NUMBER;dot++){
       if(dot<=TS_LED_NUMBER-2){
         TurnSignalStrip[dot] = CHSV( TURN_HUE, TURN_SAT, brightens2[0]);
         TurnSignalStrip[dot+1] = CHSV( TURN_HUE, TURN_SAT, brightens2[1]);
@@ -110,14 +86,13 @@ void loop() {
         currentMillis = millis();
       }
     }
-  }
-  else if(TurnSignalState == LOW){
-    fill_solid(TurnSignalStrip,TS_LED_NUMBER,CRGB::Black);
-    FastLED[TURN_INDEX].showLeds();
-  }
-  
-  if(StopSignalState == HIGH){
-    if(fStop){
+}
+void TurnSignalAnimationOFF(){
+  fill_solid(TurnSignalStrip,TS_LED_NUMBER,CRGB::Black);
+  FastLED[TURN_INDEX].showLeds();
+}
+void StopSignalAnimationON(){
+  if(fStop){
       triggerOnce = true;
       for(int i=0;i<15;i++){
         if(i%2 !=0){
@@ -134,19 +109,17 @@ void loop() {
       fill_solid(StopSignalStrip,SS_LED_NUMBER,CHSV(STOP_HUE,STOP_SAT,brightens2[0]));
       FastLED[STOP_INDEX].showLeds();
     }
-    // if X time passed after pedal is DEpressed or hold - make animation active again, else static light
-  }
-  else if(StopSignalState == LOW){
-    fill_solid(StopSignalStrip,SS_LED_NUMBER,CRGB::Black);
+}
+void StopSignalAnimationOFF(){
+  fill_solid(StopSignalStrip,SS_LED_NUMBER,CRGB::Black);
     FastLED[STOP_INDEX].showLeds();
     if(triggerOnce){
       STOPTimer.start();
       triggerOnce = false;
     }
-  }
-  
-  if(RunSignalState == HIGH){
-    if(onStartupRunning){
+}
+void RunSignalAnimationON(){
+  if(onStartupRunning){
       for(int i=0;i<10;i++){
         fill_solid(RunningStrip,RS_LED_NUMBER,CHSV(RUN_HUE,RUN_SAT,i*25));
         currentMillis = previousMillis = millis();
@@ -159,12 +132,60 @@ void loop() {
     }else{
       fill_solid(RunningStrip,RS_LED_NUMBER,CHSV(RUN_HUE,RUN_SAT,255));
       FastLED[RUN_INDEX].showLeds();
-    }    
+    }
+}
+void RunSignalAnimationOFF(){
+  onStartupRunning = true;
+    fill_solid(RunningStrip,RS_LED_NUMBER,CRGB::Black);
+    FastLED[RUN_INDEX].showLeds(); 
+}
+// FUNCTIONS
+
+void setup() {
+  pinMode(TURNS_INPUT_PIN,INPUT);
+  pinMode(STOPS_INPUT_PIN,INPUT);
+  pinMode(RUNS_INPUT_PIN,INPUT);
+
+  pinMode(TURNS_OUTPUT_PIN,OUTPUT);
+  pinMode(STOPS_OUTPUT_PIN,OUTPUT);
+  pinMode(RUNS_OUTPUT_PIN,OUTPUT);
+
+  FastLED.addLeds<LED_TYPE, TURNS_OUTPUT_PIN, COLOR_ORDER>(TurnSignalStrip, TS_LED_NUMBER).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, STOPS_OUTPUT_PIN, COLOR_ORDER>(StopSignalStrip, SS_LED_NUMBER).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<LED_TYPE, RUNS_OUTPUT_PIN, COLOR_ORDER>(RunningStrip, RS_LED_NUMBER).setCorrection(TypicalLEDStrip);
+
+  fill_solid(TurnSignalStrip,TS_LED_NUMBER,CRGB::Black);
+  fill_solid(StopSignalStrip,SS_LED_NUMBER,CRGB::Black);
+  fill_solid(RunningStrip,RS_LED_NUMBER,CRGB::Black);
+
+  StartUpAnimation();
+}
+
+void loop() {
+  TurnSignalState = digitalRead(TURNS_INPUT_PIN);
+  StopSignalState = digitalRead(STOPS_INPUT_PIN);
+  RunSignalState = digitalRead(RUNS_INPUT_PIN);
+
+  if(TurnSignalState == HIGH){
+    TurnSignalAnimationON();
+  }
+  else if(TurnSignalState == LOW){
+    TurnSignalAnimationOFF();
+  }
+  
+  if(StopSignalState == HIGH){
+    StopSignalAnimationON();
+    // if X time passed after pedal is DEpressed or hold - make animation active again, else static light
+  }
+  else if(StopSignalState == LOW){
+    StopSignalAnimationOFF();
+  }
+  
+  if(RunSignalState == HIGH){
+    RunSignalAnimationON();    
   }
   else if(RunSignalState == LOW){
-    onStartupRunning = true;
-    fill_solid(RunningStrip,RS_LED_NUMBER,CRGB::Black);
-    FastLED[RUN_INDEX].showLeds();   
+    RunSignalAnimationOFF();  
   }
   
   if(STOPTimer.ding()){
